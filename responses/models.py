0ee40795f9d8de6d3a5dbbf5b76b137c973748f6
Rafael Sandrini <rafael@systembrasil.com.br>
2012-08-10 09:41:29 -0300

    Reorganizando - Traduzindo - Novos campos

--- responses/models.py
+++
@@ -1,3 +0,0 @@
-# This is a placeholder app, to hold all of the dynamic survey response models
-# from the surveymaker app. This was recommended on the Django wiki, and I 
-# haven't yet confirmed if this is necessary.
