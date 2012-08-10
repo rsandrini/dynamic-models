8a3b78303b8f04a8b0220e24ab979575af5916f2
Rafael Sandrini <rafael@systembrasil.com.br>
2012-08-10 10:55:00 -0300

    Ajuste Heroku

--- dynamic_models/settings/default.py
+++ dynamic_models/settings/default.py
@@ -170,38 +170,3 @@ if not 'default' in DATABASES:
 
 DATABASES['default'].update(dj_database_url.config(default='postgres://'))
 
-import sys
-import urlparse
-
-# Register database schemes in URLs.
-urlparse.uses_netloc.append('mysql')
-
-try:
-
-    # Check to make sure DATABASES is set in settings.py file.
-    # If not default to {}
-
-    if 'DATABASES' not in locals():
-        DATABASES = {}
-
-    if 'DATABASE_URL' in os.environ:
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-
-        # Ensure default database exists.
-        DATABASES['default'] = DATABASES.get('default', {})
-
-        # Update with environment configuration.
-        DATABASES['default'].update({
-            'NAME': url.path[1:],
-            'USER': url.username,
-            'PASSWORD': url.password,
-            'HOST': url.hostname,
-            'PORT': url.port,
-        })
-    
-
-        if url.scheme == 'mysql':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-except Exception:
-    print 'Unexpected error:', sys.exc_info()
-
